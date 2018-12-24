caa4c1ab778d5d9f41921bbd45336235b424636b
Oleg Hansen <oleg-ucb@outlook.com>
2018-12-24 14:14:39 -0800

    this commit is fix heroku deployment issues

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Le Big Mac');
+INSERT INTO burgers (name, devoured) VALUES ('Kobe Slider', true);
+INSERT INTO burgers (name, devoured) VALUES ('Ostrich', true);
+INSERT INTO burgers (name, devoured) VALUES ('Big Kahuna', true);
+INSERT INTO burgers (name) VALUES ('Rata');
